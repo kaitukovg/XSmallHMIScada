4549ec7a8afad5d2a9026555d4f29beebeb7aea0
kaitukovg <germonskiy@gmail.com>
2025-12-21 22:52:59 +0300

    Update HistoryGraph.h

--- include/HistoryGraph.h
+++ include/HistoryGraph.h
@@ -6,31 +6,22 @@
 #include <SFML/Graphics.hpp>
 #include <vector>
 
-// График, показывающий историю изменений переменной
-// Например, как менялась температура за последние N секунд
-
 class HistoryGraph : public VisualObject {
 private:
-    // Размеры графика
+
     float width, height;
-    
-    // Фон графика
+
     sf::RectangleShape background;
     
-    // Имя переменной, историю которой показываем
     std::string variableName;
     
-    // Максимальное количество точек на графике
     size_t maxHistorySize;
     
-    // Цвет линии графика
     sf::Color lineColor;
     
-    // Цвет сетки
     sf::Color gridColor;
 
 public:
-    // Конструктор
     HistoryGraph(float x, float y, float width, float height,
                  const std::string& name, VariableDatabase* db,
                  const std::string& varName = "", 
@@ -38,15 +29,15 @@ public:
                  const sf::Color& lineClr = sf::Color::Blue,
                  const sf::Color& gridClr = sf::Color(200, 200, 200, 100));
     
-    // Методы родителя
+
     void draw(sf::RenderWindow& window) override;
     void update() override;
     
 private:
-    // Рисует сетку графика
+
     void drawGrid(sf::RenderWindow& window);
     
-    // Рисует сам график (линию)
+
     void drawGraph(sf::RenderWindow& window);
 };
 
