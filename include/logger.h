242bf7f815db90128de67d9ca00874462f80f15e
kaitukovg <germonskiy@gmail.com>
2025-12-21 23:00:23 +0300

    Update logger.h

--- include/logger.h
+++ include/logger.h
@@ -4,21 +4,16 @@
 #include <iostream>
 #include <string>
 
-// Простейший логгер - выводит сообщения в консоль с разными уровнями важности
-
 class Logger {
 public:
-    // Информационное сообщение (обычные события)
     static void info(const std::string& message) {
         std::cout << "[INFO] " << message << std::endl;
     }
     
-    // Сообщение об ошибке (критические проблемы)
     static void error(const std::string& message) {
         std::cerr << "[ERROR] " << message << std::endl;
     }
     
-    // Предупреждение (некритические проблемы)
     static void warning(const std::string& message) {
         std::cout << "[WARNING] " << message << std::endl;
     }
