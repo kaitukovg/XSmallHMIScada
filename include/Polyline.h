6a8c26c0a55f3371a06125b6853dc5be914464ec
kaitukovg <germonskiy@gmail.com>
2025-12-21 22:57:01 +0300

    Update Polyline.h

--- include/Polyline.h
+++ include/Polyline.h
@@ -6,31 +6,22 @@
 #include <SFML/Graphics.hpp>
 #include <vector>
 
-// Ломаная линия (последовательность соединенных отрезков)
-// Может обновляться динамически из истории переменной
-
 class Polyline : public VisualObject {
 private:
-    // Массив точек (вершин) ломаной
     std::vector<sf::Vertex> points;
     
-    // Цвет линии
     sf::Color color;
     
-    // Имя переменной, из которой берутся данные
     std::string variableName;
 
 public:
-    // Конструктор
     Polyline(const std::vector<sf::Vector2f>& points, 
              const sf::Color& color, const std::string& name,
              VariableDatabase* db, const std::string& varName = "");
     
-    // Методы родителя
     void draw(sf::RenderWindow& window) override;
     void update() override;
     
-    // Обновляет точки ломаной (вручную)
     void updatePoints(const std::vector<sf::Vector2f>& newPoints);
 };
 
