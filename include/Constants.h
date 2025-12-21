50ddaacc75b11b645f6d85500ad9f60b068ef4f4
kaitukovg <germonskiy@gmail.com>
2025-12-21 22:52:19 +0300

    Update Constants.h

--- include/Constants.h
+++ include/Constants.h
@@ -3,16 +3,12 @@
 
 #include <string>
 
-/**
- * Централизованное хранение констант проекта.
- * Упрощает изменение настроек и поддержку кода.
- */
 namespace Constants {
-    const int WINDOW_WIDTH = 1024;  // Ширина основное окна 
-    const int WINDOW_HEIGHT = 768;  // Высота основного окна
-    const std::string FONT_PATH = "assets/fonts/helveticabold.ttf";  // Основной шрифт
+    const int WINDOW_WIDTH = 1024;   
+    const int WINDOW_HEIGHT = 768;  
+    const std::string FONT_PATH = "assets/fonts/helveticabold.ttf"; 
 
-    // Для статической/динамической сборки
+
     #ifdef SFML_STATIC
         const bool IS_STATIC_BUILD = true;
     #else
