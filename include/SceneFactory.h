c06a5bdc91a9d213b0c358a0026443b497a9452d
kaitukovg <germonskiy@gmail.com>
2025-12-21 22:58:32 +0300

    Update SceneFactory.h

--- include/SceneFactory.h
+++ include/SceneFactory.h
@@ -5,20 +5,15 @@
 #include <memory>
 #include <string>
 
-// Предварительные объявления (чтобы не включать заголовочные файлы)
 class VariableDatabase;
 class VisualObject;
 
-// Пространство имен SFML
 namespace sf {
     class Font;
 }
 
-// Фабрика сцен - создает наборы визуальных объектов
-
 class SceneFactory {
 public:
-    // Создает тестовую сцену для демонстрации возможностей
     static std::vector<std::unique_ptr<VisualObject>> createDemoScene(
         VariableDatabase* db,
         sf::Font* font);
