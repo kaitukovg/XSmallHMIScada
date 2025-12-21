c68ab0377210dff28626bee60163f020639e9cc4
kaitukovg <germonskiy@gmail.com>
2025-12-21 23:14:15 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,19 @@
 #include "HmiPlayer.h"
 #include "logger.h"
 
-// Главная функция, с которой начинается выполнение программы
 int main() {
     Logger::info("LAUNCHING XSmall-HMI SCADA Player...");
     
-    // Создаем главный объект приложения
     HmiPlayer player;
     
-    // Пробуем инициализировать
     if (!player.initialize()) {
         Logger::error("FAILED TO INITIALIZED HMI Player");
-        return -1;  // Возвращаем код ошибки
+        return -1;  
     }
     
     Logger::info("HMI Player SUCCESSFULLY INITIALIZED. CONGRATULATIONS!");
-    player.run();  // Запускаем главный цикл
+    player.run();  
     
-    return 0;  // Успешное завершение
+    return 0;  
 }
+
