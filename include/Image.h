6d1bb396d135c7f915a9bbf139f2616ce4e9ba59
kaitukovg <germonskiy@gmail.com>
2025-12-21 22:54:19 +0300

    Update Image.h

--- include/Image.h
+++ include/Image.h
@@ -4,36 +4,25 @@
 #include "VisualObject.h"
 #include <SFML/Graphics.hpp>
 
-// Класс для отображения изображений
-
 class Image : public VisualObject {
 private:
-    // Текстура (само изображение)
     sf::Texture texture;
-    
-    // Спрайт для отображения текстуры
     sf::Sprite sprite;
     
-    // Размеры изображения
     float imgWidth, imgHeight;
     
-    // Путь к файлу изображения
     std::string imagePath;
     
-    // Удалось ли загрузить текстуру
     bool textureLoaded;
 
 public:
-    // Конструктор
+
     Image(float x, float y, float width, float height,
-          const std::string& path, const std::string& name,
-          VariableDatabase* db);
+          const std::string& path, const std::string& name, VariableDatabase* db);
     
-    // Методы родителя
     void draw(sf::RenderWindow& window) override;
-    void update() override {};  // Изображение не обновляется
+    void update() override {}; 
     
-    // Загружает текстуру из файла
     bool loadTexture(const std::string& path);
 };
 
