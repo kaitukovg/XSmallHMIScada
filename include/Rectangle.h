3857fcd80041d246ad2e798d49a6cbe3d0e96d20
kaitukovg <germonskiy@gmail.com>
2025-12-21 22:57:47 +0300

    Update Rectangle.h

--- include/Rectangle.h
+++ include/Rectangle.h
@@ -5,43 +5,31 @@
 #include "VariableDatabase.h"
 #include <SFML/Graphics.hpp>
 
-// Прямоугольник - может менять цвет в зависимости от значения переменной
-// Используется для индикации состояния (например, зелёный - норма, красный - авария)
-
 class Rectangle : public VisualObject {
 private:
-    // Размеры прямоугольника
     float width, height;
     
-    // SFML объект для отрисовки
     sf::RectangleShape shape;
     
-    // Цвет по умолчанию
     sf::Color defaultColor;
     
-    // Имя связанной переменной (может быть пустым)
     std::string variableName;
     
-    // Структура для условия изменения цвета
     struct ColorCondition {
-        double value;     // При каком значении переменной
-        sf::Color color;  // Какой цвет установить
+        double value;    
+        sf::Color color; 
     };
     
-    // Список условий (если переменная = значению -> установить цвет)
     std::vector<ColorCondition> conditions;
 
 public:
-    // Конструктор
     Rectangle(float x, float y, float width, float height, 
               const sf::Color& color, const std::string& name,
               VariableDatabase* db, const std::string& varName = "");
     
-    // Методы родителя
     void draw(sf::RenderWindow& window) override;
     void update() override;
 
-    // Добавляет условие изменения цвета
     void addCondition(double value, const sf::Color& color);
 };
 
