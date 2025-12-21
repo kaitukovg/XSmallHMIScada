0953a10d7547e78b7b68e61c560cf23d7ea279a9
kaitukovg <germonskiy@gmail.com>
2025-12-21 23:13:33 +0300

    Update VisualObject.cpp

--- src/VisualObject.cpp
+++ src/VisualObject.cpp
@@ -1,20 +1,13 @@
 #include "VisualObject.h"
 
-// Конструктор просто сохраняет переданные значения в поля класса
 VisualObject::VisualObject(float x, float y, const std::string& name, VariableDatabase* db)
     : x(x), y(y), name(name), database(db) {
-    // Иинициализируем поля
-    // x(x) означает: поле x класса получает значение параметра x
 }
 
-// Меняем позицию объекта
 void VisualObject::setPosition(float newX, float newY) {
-    x = newX;  // Запоминаем новую X координату
-    y = newY;  // Запоминаем новую Y координату
+    x = newX;  
+    y = newY; 
 }
-
-// Возвращаем имя объекта
 std::string VisualObject::getName() const {
-    return name;  // Просто возвращаем то, что хранится в поле name
-
+    return name;  
 }
